e85cc9cd8333448547d20e9f1ab9b06d1d308430
sohaib-cyborg <ashrafsohaib19@gmail>
2024-08-08 14:22:14 +0500

    last commit

--- __test__/itemCatFlow.test.js
+++ __test__/itemCatFlow.test.js
@@ -1,10 +1,13 @@
 import { act, fireEvent, render,screen } from "@testing-library/react";
-import MockData from "../Mocks/ResListData.json";
+import MockData from "../Mocks/ResMenu.json";
 import Resdata from "../src/pages/RestaurantData";
 import { Provider } from "react-redux";
 import AppStore from "../utils/appstore";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
+import Header from "../src/components/Header";
+import Cart from "../src/pages/cart";
+
 
 global.fetch = jest.fn(() => {
   return Promise.resolve({
@@ -19,15 +22,29 @@ test("should load the recommend menu and update the cart accordingly", async () 
     render(
     <BrowserRouter>    
     <Provider store={AppStore}>
+    <Header/>
     <Resdata/>
+    <Cart/>
     </Provider>
     </BrowserRouter>
     );
     
   });
   
- const accordianView = screen.getByText("Recommended-(15)");
+ const accordianView = screen.getByText("Recommended-(14)");
  fireEvent.click(accordianView);
+ expect(screen.getAllByTestId("fooditems").length).toBe(14);
+ const adBtns = screen.getAllByRole("button",{name:"ADD"});
+ expect(screen.getByText("Cart-(0)")).toBeInTheDocument();
+ 
+ fireEvent.click(adBtns[0]);
+ expect(screen.getByText("Cart-(1)")).toBeInTheDocument();
+ 
  expect(screen.getAllByTestId("fooditems").length).toBe(15);
+ fireEvent.click(screen.getByRole("button",{name:"Clear"}));
+ expect(screen.getAllByTestId("fooditems").length).toBe(14);
 
 });
+//the above test suit check the whole flow of the cart
+// first it fetches the heading like recommended, then clicks on it to show the items and then clicks on the items to add it to the cart
+//this is the last commit!
--- src/pages/RestaurantData.js
+++ src/pages/RestaurantData.js
@@ -26,7 +26,7 @@ const Resdata=()=>{
 
      
  if(restData === null)return <Shimmer/>;
- 
+ console.log(restData); 
  const {name,costForTwoMessage,cuisines} = restData?.cards[2]?.card?.card?.info;
  //const {itemCards} = restData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
  const itemCategories = restData?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c)=>c.card?.card?.["@type"]==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
